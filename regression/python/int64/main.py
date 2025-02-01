cb3d1d3276f1e9c1c7e3ad92f49ef095ceb0707e
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-02-01 16:48:16 +0000

    add failing test

---
+++ regression/python/int64/main.py
@@ -0,0 +1,10 @@
+x = 8589934592
+assert x == 8589934592
+
+# __ESBMC_main (__ESBMC_main):
+#         // 17 file main.py line 1 column 0
+#         ASSIGN x=0;
+#         // 18 no location
+#         ASSERT x == 0
+#         // 19 no location
+#         END_FUNCTION
