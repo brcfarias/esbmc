21eb14065a160c08516cf51936fe60724e6b1c70
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-02-26 17:42:43 +0000

    add test

---
+++ regression/numpy/broadcasting/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#array A with (2,) shape
+A = np.array([1,2])
+
+#array B with (3,) shape
+B = np.array([10,20,30])
+
+# This sum generates error, as the shape (2,) and (3,) are not compatible
+#C = A + B
+C = np.add(A,B)
+
