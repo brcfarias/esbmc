ddeb57edcb0ba3b580f4d74d5e09ab51d5d2e1b5
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-02-28 09:32:18 +0000

    fix test

--- regression/numpy/broadcasting/main.py
+++ regression/numpy/broadcasting/main.py
@@ -7,6 +7,7 @@ A = np.array([1,2])
 B = np.array([10,20,30])
 
 # This sum generates error, as the shape (2,) and (3,) are not compatible
-#C = A + B
 C = np.add(A,B)
 
+#assert C[0] == 11
+#assert C[1] == 22
