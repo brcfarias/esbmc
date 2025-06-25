c36719b49f972aeb2a9246884f5652506854e7c4
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-06-25 18:54:13 +0100

    Removing uneeded inclusions

--- src/esbmc/main.cpp
+++ src/esbmc/main.cpp
@@ -1,9 +1,4 @@
-#include <cstdint>
 #include <esbmc/esbmc_parseoptions.h>
-#include <langapi/mode.h>
-
-#include <irep2/irep2.h>
-#include <util/config.h>
 
 int main(int argc, const char **argv)
 {
