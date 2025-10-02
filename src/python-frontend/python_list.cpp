987cab74ff66ca4a113978359222e13136df4a28
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-10-02 11:09:08 +0100

    clean

--- src/python-frontend/python_list.cpp
+++ src/python-frontend/python_list.cpp
@@ -280,8 +280,9 @@ exprt python_list::index(const exprt &array, const nlohmann::json &slice_node)
   return exprt();
 }
 
-exprt python_list::remove(const exprt &array, const exprt &value) {
-	return exprt();
+exprt python_list::remove(const exprt &array, const exprt &value)
+{
+  return exprt();
 }
 
 exprt python_list::handle_range_slice(
